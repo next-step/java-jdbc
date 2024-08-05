45712a8f89a61e1f6b9bd8158fa77708f195eb00
jinyoung <jinyoungchoi95@gmail.com>
2024-08-06 02:30:49 +0900

    refactor: rowMapper 메소드 분리

--- app/src/main/java/camp/nextstep/dao/UserDao.java
+++ app/src/main/java/camp/nextstep/dao/UserDao.java
@@ -2,6 +2,7 @@ package camp.nextstep.dao;
 
 import camp.nextstep.domain.User;
 import camp.nextstep.jdbc.core.JdbcTemplate;
+import camp.nextstep.jdbc.core.RowMapper;
 import com.interface21.beans.factory.annotation.Autowired;
 import com.interface21.context.stereotype.Repository;
 import org.slf4j.Logger;
@@ -68,33 +69,29 @@ public class UserDao {
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
 
-        return jdbcTemplate.query(sql, rs -> new User(
-                rs.getLong("id"),
-                rs.getString("account"),
-                rs.getString("password"),
-                rs.getString("email")
-        ));
+        return jdbcTemplate.query(sql, userRowMapper());
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
 
-        return jdbcTemplate.queryForObject(sql, rs -> new User(
-                rs.getLong("id"),
-                rs.getString("account"),
-                rs.getString("password"),
-                rs.getString("email")
-        ), id).orElseThrow(() -> new IllegalArgumentException("not found entity"));
+        return jdbcTemplate.queryForObject(sql, userRowMapper(), id)
+                .orElseThrow(() -> new IllegalArgumentException("not found entity"));
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
 
-        return jdbcTemplate.queryForObject(sql, rs -> new User(
+        return jdbcTemplate.queryForObject(sql, userRowMapper(), account)
+                .orElseThrow(() -> new IllegalArgumentException("not found entity"));
+    }
+
+    private RowMapper<User> userRowMapper() {
+        return rs -> new User(
                 rs.getLong("id"),
                 rs.getString("account"),
                 rs.getString("password"),
                 rs.getString("email")
-        ), account).orElseThrow(() -> new IllegalArgumentException("not found entity"));
+        );
     }
 }
