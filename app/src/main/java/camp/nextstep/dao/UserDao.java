79f02c012a11ba78ae886f217cb26ec612a8c897
jinyoung <jinyoungchoi95@gmail.com>
2024-08-06 19:29:53 +0900

    refactor: optimize import

--- app/src/main/java/camp/nextstep/dao/UserDao.java
+++ app/src/main/java/camp/nextstep/dao/UserDao.java
@@ -9,11 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
