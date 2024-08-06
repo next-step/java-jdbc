b6ef35785b9d2aaf16f5edd8e654a748b1794ff8
SeolYoungKim <nasur4da@gmail.com>
2024-08-06 23:28:50 +0900

    refactor(JdbcTemplate): insert 메서드 이름을 update로 변경하고, 리소스를 해제하는 로직들을 메서드로 분리

--- jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
@@ -4,13 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JdbcTemplate {
-
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
     private final DataSource dataSource;
@@ -19,7 +19,7 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void insert(String sql, List<?> params) {
+    public void update(String sql, List<?> params) {
         printLog(sql, params);
 
         Connection conn = null;
@@ -34,17 +34,8 @@ public class JdbcTemplate {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
+            postProcessing(pstmt);
+            postProcessing(conn);
         }
     }
 
@@ -69,25 +60,40 @@ public class JdbcTemplate {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
+            postProcessing(rs);
+            postProcessing(pstmt);
+            postProcessing(conn);
+        }
+    }
 
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
+    public <T> List<T> select(String sql, ResultSetHandler<T> resultSetHandler) {
+        return select(sql, List.of(), resultSetHandler);
+    }
 
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
+    public <T> List<T> select(String sql, List<?> params, ResultSetHandler<T> resultSetHandler) {
+        printLog(sql, params);
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            setUpParameters(pstmt, params);
+
+            rs = pstmt.executeQuery();
+            List<T> results = new ArrayList<>();
+            while (rs.next()) {
+                results.add(resultSetHandler.handle(rs));
             }
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            postProcessing(rs);
+            postProcessing(pstmt);
+            postProcessing(conn);
         }
     }
 
@@ -98,6 +104,31 @@ public class JdbcTemplate {
     private void setUpParameters(PreparedStatement pstmt, List<?> params) throws SQLException {
         for (int i = 0; i < params.size(); i++) {
             pstmt.setObject(i + 1, params.get(i));
+        }
+    }
+
+    private void postProcessing(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ignored) {}
+    }
+
+    private void postProcessing(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException ignored) {}
+    }
+
+    private void postProcessing(PreparedStatement pstmt) {
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException ignored) {
         }
     }
 }
