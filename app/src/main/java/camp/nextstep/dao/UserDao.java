d5e2a6d285f0a0d0f51cd85f51bfe0e27cfc4d98
SeolYoungKim <nasur4da@gmail.com>
2024-08-06 23:29:01 +0900

    feat(JdbcTemplate): 업데이트 로직 구현

--- app/src/main/java/camp/nextstep/dao/UserDao.java
+++ app/src/main/java/camp/nextstep/dao/UserDao.java
@@ -4,6 +4,7 @@ import camp.nextstep.domain.User;
 import camp.nextstep.jdbc.core.JdbcTemplate;
 import com.interface21.beans.factory.annotation.Autowired;
 import com.interface21.context.stereotype.Repository;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
@@ -29,28 +30,30 @@ public class UserDao {
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-        jdbcTemplate.insert(sql, List.of(user.getAccount(), user.getPassword(), user.getEmail()));
+        jdbcTemplate.update(sql, List.of(user.getAccount(), user.getPassword(), user.getEmail()));
     }
 
     public void update(final User user) {
-        // todo
+        String sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        jdbcTemplate.update(sql, List.of(user.getAccount(), user.getPassword(), user.getEmail(), user.getId()));
     }
 
     public List<User> findAll() {
-        // todo
-        return null;
+        String sql = "select id, account, password, email from users";
+        return jdbcTemplate.select(sql, (rs) -> {
+            try {
+                return createUser(rs);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
         return jdbcTemplate.selectOne(sql, List.of(id), (rs) -> {
             try {
-                return new User(
-                        rs.getInt("id"),
-                        rs.getString("account"),
-                        rs.getString("password"),
-                        rs.getString("email")
-                );
+                return createUser(rs);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
@@ -61,15 +64,19 @@ public class UserDao {
         final var sql = "select id, account, password, email from users where account = ?";
         return jdbcTemplate.selectOne(sql, List.of(account), (rs) -> {
             try {
-                return new User(
-                        rs.getInt("id"),
-                        rs.getString("account"),
-                        rs.getString("password"),
-                        rs.getString("email")
-                );
+                return createUser(rs);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    private User createUser(ResultSet rs) throws SQLException {
+        return new User(
+                rs.getInt("id"),
+                rs.getString("account"),
+                rs.getString("password"),
+                rs.getString("email")
+        );
     }
 }
