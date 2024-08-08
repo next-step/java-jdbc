75dd6cefb4af183e210308d735cfbded2b537253
jinyoung <jinyoungchoi95@gmail.com>
2024-08-09 01:25:33 +0900

    refactor: JdbcTemplate에 PreparedStatementSetter/ResultSetSetter 적용

--- jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
@@ -1,16 +1,11 @@
 package camp.nextstep.jdbc.core;
 
-import camp.nextstep.dao.DataAccessException;
 import camp.nextstep.dao.QueryFormatException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,25 +16,35 @@ public class JdbcTemplate {
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
     private static final String QUERY_PLACEHOLDER = "?";
 
-    private final DataSource dataSource;
+    private final PreparedStatementSetter preparedStatementSetter;
+    private final ResultSetSetter resultSetSetter;
 
     public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.preparedStatementSetter = new DefaultPreparedStatementSetter(dataSource);
+        this.resultSetSetter = new DefaultResultSetSetter();
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
         validateSql(sql, args);
-        return executeQuery(sql, preparedStatement -> parse(rowMapper, preparedStatement), args);
+        return preparedStatementSetter.executeQuery(
+                sql,
+                preparedStatement -> resultSetSetter.parse(rowMapper, preparedStatement),
+                args
+        );
     }
 
     public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
         validateSql(sql, args);
-        return executeQuery(sql, preparedStatement -> parseToObject(rowMapper, preparedStatement), args);
+        return preparedStatementSetter.executeQuery(
+                sql,
+                preparedStatement -> resultSetSetter.parseToObject(rowMapper, preparedStatement),
+                args
+        );
     }
 
     public int update(String sql, Object... args) {
         validateSql(sql, args);
-        return executeQuery(sql, PreparedStatement::executeUpdate, args);
+        return preparedStatementSetter.executeQuery(sql, PreparedStatement::executeUpdate, args);
     }
 
     private void validateSql(String sql, Object... args) {
@@ -56,56 +61,6 @@ public class JdbcTemplate {
     private void validateSqlArguments(String sql, Object... args) {
         if (countContainSequence(sql, QUERY_PLACEHOLDER) != args.length) {
             throw new QueryFormatException("sql의 placeholder 수에 맞는 파라미터가 필요합니다.");
-        }
-    }
-
-    private <T> T executeQuery(String sql, PreparedStatementParser<T> preparedStatementParser, Object... args) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            prepareArguments(preparedStatement, args);
-
-            return preparedStatementParser.parse(preparedStatement);
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
-    }
-
-    private void prepareArguments(PreparedStatement preparedStatement, Object... args) throws SQLException {
-        for (int i = 1; i <= args.length; i++) {
-            preparedStatement.setObject(i, args[i - 1]);
-        }
-    }
-
-    private <T> List<T> parse(RowMapper<T> rowMapper, PreparedStatement preparedStatement) throws SQLException {
-        return parseResultSet(parse(rowMapper), preparedStatement);
-    }
-
-    private <T> ResultSetParser<List<T>> parse(RowMapper<T> rowMapper) {
-        return resultSet -> {
-            List<T> result = new ArrayList<>();
-            while (resultSet.next()) {
-                result.add(rowMapper.mapRow(resultSet));
-            }
-            return result;
-        };
-    }
-
-    private <T> Optional<T> parseToObject(RowMapper<T> rowMapper, PreparedStatement preparedStatement) throws SQLException {
-        return parseResultSet(parseToObject(rowMapper), preparedStatement);
-    }
-
-    private <T> ResultSetParser<Optional<T>> parseToObject(RowMapper<T> rowMapper) {
-        return resultSet -> {
-            if (resultSet.next()) {
-                return Optional.of(rowMapper.mapRow(resultSet));
-            }
-            return Optional.empty();
-        };
-    }
-
-    private <T> T parseResultSet(ResultSetParser<T> resultSetParser, PreparedStatement preparedStatement) throws SQLException {
-        try (ResultSet resultSet = preparedStatement.executeQuery()) {
-            return resultSetParser.parse(resultSet);
         }
     }
 }
