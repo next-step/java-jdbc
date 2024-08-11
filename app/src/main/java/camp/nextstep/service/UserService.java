bf5b2391ca6421c53ae5745872915abb221b5c7f
jinyoung <jinyoungchoi95@gmail.com>
2024-08-12 02:17:28 +0900

    feat: transaction 적용

--- app/src/main/java/camp/nextstep/service/UserService.java
+++ app/src/main/java/camp/nextstep/service/UserService.java
@@ -1,5 +1,6 @@
 package camp.nextstep.service;
 
+import camp.nextstep.dao.DataAccessException;
 import camp.nextstep.dao.UserDao;
 import camp.nextstep.dao.UserHistoryDao;
 import camp.nextstep.domain.User;
@@ -7,16 +8,22 @@ import camp.nextstep.domain.UserHistory;
 import com.interface21.beans.factory.annotation.Autowired;
 import com.interface21.context.stereotype.Service;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @Service
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final DataSource dataSource;
 
     @Autowired
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final DataSource dataSource) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.dataSource = dataSource;
     }
 
     public User findByAccount(final String account) {
@@ -31,10 +38,23 @@ public class UserService {
         userDao.insert(user);
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+    public void changePassword(final long id, final String newPassword, final String createBy) throws SQLException {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+
+            connection.commit();
+        } catch (Exception e) {
+            connection.rollback();
+            throw new DataAccessException(e);
+        } finally {
+            connection.close();
+        }
     }
 }
