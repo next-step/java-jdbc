4a2ea522958944f7ccf3f9d377f86ab46f9ab2cb
Jongmin Yoon <jongmin4943@gmail.com>
2024-08-12 12:06:42 +0900

    feat(JdbcTemplate): find 쿼리 실행을 위한 query 메서드 추가

--- jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
@@ -6,7 +6,10 @@ import org.slf4j.LoggerFactory;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
 
@@ -34,4 +37,53 @@ public class JdbcTemplate {
             throw new DataAccessException(e);
         }
     }
+
+    public <T> List<T> query(final String sql, final PreparedStatementSetter preparedStatementSetter, final RowMapper<T> rowMapper) {
+        ResultSet resultSet = null;
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            if (preparedStatementSetter != null) {
+                preparedStatementSetter.setValues(pstmt);
+            }
+
+            resultSet = pstmt.executeQuery();
+
+            log.info("query : {}", sql);
+
+            final List<T> result = new ArrayList<>();
+            while (resultSet.next()) {
+                result.add(rowMapper.mapRow(resultSet));
+            }
+            return result;
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        } finally {
+            closeResultSet(resultSet);
+        }
+    }
+
+    private void closeResultSet(final ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
+        return query(sql, null, rowMapper);
+    }
+
+    public <T> T queryForObject(final String sql, final PreparedStatementSetter preparedStatementSetter, final RowMapper<T> rowMapper) {
+        final List<T> result = query(sql, preparedStatementSetter, rowMapper);
+        if (result.isEmpty()) {
+            return null;
+        }
+        return result.get(0);
+    }
+
 }
--- jdbc/src/test/java/camp/nextstep/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/camp/nextstep/jdbc/core/JdbcTemplateTest.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.Test;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.doThrow;
@@ -20,6 +23,7 @@ class JdbcTemplateTest {
     private final DataSource dataSource = mock(DataSource.class);
     private final Connection connection = mock(Connection.class);
     private final PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    private final ResultSet resultSet = mock(ResultSet.class);
 
     @Test
     @DisplayName("update 시 쿼리가 정상적으로 실행되고 리소스를 회수한다.")
@@ -53,5 +57,75 @@ class JdbcTemplateTest {
 
         verify(preparedStatement).close();
         verify(connection).close();
+    }
+
+
+    @Test
+    @DisplayName("query 실행 시 객체들을 조회 후 리소스를 반환한다.")
+    void testQuery() throws SQLException {
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next())
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(false);
+        when(resultSet.getLong(1))
+                .thenReturn(1L)
+                .thenReturn(2L);
+        when(resultSet.getString(2))
+                .thenReturn("test")
+                .thenReturn("test2");
+        when(resultSet.getString(3))
+                .thenReturn("password")
+                .thenReturn("password");
+        when(resultSet.getString(4))
+                .thenReturn("test@test.com")
+                .thenReturn("test2@test.com");
+
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        final String sql = "SELECT id, account, password, email FROM users";
+        final List<User> users = jdbcTemplate.query(sql, rs -> new User(
+                rs.getLong(1),
+                rs.getString(2),
+                rs.getString(3),
+                rs.getString(4)
+        ));
+
+        assertThat(users).containsExactly(
+                new User(1L, "test", "password", "test@test.com"),
+                new User(2L, "test2", "password", "test2@test.com")
+        );
+
+        verify(resultSet).close();
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    @Test
+    @DisplayName("query 실행 시 SQLException 발생 시 DataAccessException 으로 예외를 던지고, 리소스를 회수한다.")
+    void testQueryException() throws SQLException {
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+        doThrow(new SQLException()).when(preparedStatement).executeQuery();
+
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        assertThatThrownBy(() -> jdbcTemplate.query(
+                "SELECT id, account, password, email FROM users",
+                rs -> new User(
+                        rs.getLong(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4))
+        )).isInstanceOf(DataAccessException.class);
+
+        verify(preparedStatement).close();
+        verify(connection).close();
+    }
+
+    private record User(Long id, String account, String password, String email) {
+
     }
 }
