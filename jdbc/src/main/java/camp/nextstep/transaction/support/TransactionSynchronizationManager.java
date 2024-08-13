d2aca5ef31431ff1e57b9be767f57aebb8bd6eea
jinyoung <jinyoungchoi95@gmail.com>
2024-08-14 03:01:44 +0900

    refactor: unused final remove

--- jdbc/src/main/java/camp/nextstep/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/camp/nextstep/transaction/support/TransactionSynchronizationManager.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public final class TransactionSynchronizationManager {
+public class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>() {
         @Override
