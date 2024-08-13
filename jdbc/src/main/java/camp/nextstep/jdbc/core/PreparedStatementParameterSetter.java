1cc2dbefab22791a9c00e4134dcc705cee76193f
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-13 13:08:20 +0900

    feat : row mapping 하는 함수와 타입별로 처리하는 enum 타입의 메서드 추가

---
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/PreparedStatementParameterSetter.java
@@ -0,0 +1,70 @@
+package camp.nextstep.jdbc.core;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.function.TriConsumer;
+
+public enum PreparedStatementParameterSetter {
+    STRING(String.class, (preparedStatement, index, value) -> {
+        try {
+            preparedStatement.setString(index, (String) value);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }),
+    LONG(Long.class, (preparedStatement, index, value) -> {
+        try {
+            preparedStatement.setLong(index, (Long) value);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }),
+    INTEGER(Integer.class, (preparedStatement, index, value) -> {
+        try {
+            preparedStatement.setInt(index, (Integer) value);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }),
+    OBJECT(Object.class, (preparedStatement, index, value) -> {
+        try {
+            preparedStatement.setObject(index, value);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }),
+    BOOLEAN(Boolean.class, (preparedStatement, index, value) -> {
+        try {
+            preparedStatement.setBoolean(index, (Boolean) value);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    });
+
+    private final Class<?> clazz;
+    private final TriConsumer<PreparedStatement, Integer, Object> setter;
+
+    private static final Map<Class<?>, PreparedStatementParameterSetter> TYPE_MAP =
+        Arrays.stream(PreparedStatementParameterSetter.values())
+            .collect(Collectors.toMap(type -> type.clazz, type -> type));
+
+    PreparedStatementParameterSetter(Class<?> clazz, TriConsumer<PreparedStatement, Integer, Object> setter) {
+        this.clazz = clazz;
+        this.setter = setter;
+    }
+
+    public static void setParameter(PreparedStatement preparedStatement, int index, Object value) {
+        PreparedStatementParameterSetter parameterSetter = TYPE_MAP.get(value.getClass());
+        if (parameterSetter == null) {
+            throw new IllegalArgumentException("존재하지 않는 타입의 필드입니다.");
+        }
+        parameterSetter.accept(preparedStatement, index, value);
+    }
+
+    public void accept(PreparedStatement preparedStatement, int index, Object value) {
+        setter.accept(preparedStatement, index, value);
+    }
+}
