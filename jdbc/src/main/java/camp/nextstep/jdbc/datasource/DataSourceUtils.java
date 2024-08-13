c2ad6822e6394f393c44101735d40c05771e96bc
jinyoung <jinyoungchoi95@gmail.com>
2024-08-14 01:57:03 +0900

    feat: releaseConnection 시 connection close 추가 및 테스트

--- jdbc/src/main/java/camp/nextstep/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/datasource/DataSourceUtils.java
@@ -25,9 +25,9 @@ public abstract class DataSourceUtils {
         }
     }
 
-    public static void releaseConnection(Connection connection, DataSource dataSource) {
+    public static void releaseConnection(DataSource dataSource) {
         try {
-            connection.close();
+            TransactionSynchronizationManager.unbindResource(dataSource);
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
         }
--- jdbc/src/test/java/camp/nextstep/jdbc/datasource/DataSourceUtilsTest.java
+++ jdbc/src/test/java/camp/nextstep/jdbc/datasource/DataSourceUtilsTest.java
@@ -10,15 +10,15 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class DataSourceUtilsTest {
 
+    private final DataSource dataSource = mock(DataSource.class);
+    private final Connection connection = mock(Connection.class);
+
     @Test
     void connection이_최초_생성인_경우_새_connection을_바인딩하여_가져온다() throws SQLException {
-        DataSource dataSource = mock(DataSource.class);
-        Connection connection = mock(Connection.class);
         when(dataSource.getConnection()).thenReturn(connection);
 
         Connection actual = DataSourceUtils.getConnection(dataSource);
@@ -26,5 +26,15 @@ class DataSourceUtilsTest {
                 () -> assertThat(actual).isEqualTo(connection),
                 () -> assertThat(TransactionSynchronizationManager.findResource(dataSource)).isEqualTo(Optional.of(connection))
         );
+    }
+
+    @Test
+    void 요청된_DataSource에_해당하는_데이터를_unbind한다() throws SQLException {
+        when(dataSource.getConnection()).thenReturn(connection);
+        doNothing().when(connection).close();
+        DataSourceUtils.getConnection(dataSource);
+
+        DataSourceUtils.releaseConnection(dataSource);
+        verify(connection).close();
     }
 }
