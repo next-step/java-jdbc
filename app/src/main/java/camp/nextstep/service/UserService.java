d0e6508c4de13893915e36a39e218598db8b1ec9
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-16 22:48:42 +0900

    feat : 패스워드 변경부분 트랜잭션 적용

--- app/src/main/java/camp/nextstep/service/UserService.java
+++ app/src/main/java/camp/nextstep/service/UserService.java
@@ -4,19 +4,25 @@ import camp.nextstep.dao.UserDao;
 import camp.nextstep.dao.UserHistoryDao;
 import camp.nextstep.domain.User;
 import camp.nextstep.domain.UserHistory;
+import camp.nextstep.jdbc.datasource.TransactionManager;
 import com.interface21.beans.factory.annotation.Autowired;
 import com.interface21.context.stereotype.Service;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 
 @Service
 public class UserService {
 
+    private final TransactionManager transactionManager;
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
     @Autowired
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final TransactionManager transactionManager) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.transactionManager = transactionManager;
     }
 
     public User findByAccount(final String account) {
@@ -32,9 +38,18 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        transactionManager.begin();
+        try {
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
+            transactionManager.commit();
+        } catch (Exception e) {
+            transactionManager.rollback();
+            throw e;
+        } finally {
+            transactionManager.close();
+        }
     }
 }
