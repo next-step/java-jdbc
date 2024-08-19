2b0ab4cd5dd5c6b266745d72c58a228b645dcc84
Jongmin Yoon <jongmin4943@gmail.com>
2024-08-19 22:05:15 +0900

    test(UserServiceTest): testTransactionRollback 에서 TxUserService 사용하도록 변경

--- app/src/test/java/camp/nextstep/service/UserServiceTest.java
+++ app/src/test/java/camp/nextstep/service/UserServiceTest.java
@@ -45,7 +45,7 @@ class UserServiceTest {
     @Test
     void testChangePassword() {
         final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
-        final var userService = new AppUserService(userDao, userHistoryDao, transactionTemplate);
+        final var userService = new AppUserService(userDao, userHistoryDao);
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -60,7 +60,10 @@ class UserServiceTest {
     void testTransactionRollback() {
         // 트랜잭션 롤백 테스트를 위해 mock으로 교체
         final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
-        final var userService = new AppUserService(userDao, userHistoryDao, transactionTemplate);
+        // 애플리케이션 서비스
+        final var appUserService = new AppUserService(userDao, userHistoryDao);
+        // 트랜잭션 서비스 추상화
+        final var userService = new TxUserService(appUserService, transactionTemplate);
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
