f73fd57c93acb0a15ddc2c0d061d529b19367e68
Hyunta <mohai2618@gmail.com>
2024-08-21 22:26:40 +0900

    feature: UserService 인터페이스로 분리

--- app/src/main/java/camp/nextstep/service/UserService.java
+++ app/src/main/java/camp/nextstep/service/UserService.java
@@ -1,53 +1,15 @@
 package camp.nextstep.service;
 
-import camp.nextstep.dao.UserDao;
-import camp.nextstep.dao.UserHistoryDao;
 import camp.nextstep.domain.User;
-import camp.nextstep.domain.UserHistory;
-import camp.nextstep.transaction.TransactionManager;
-import com.interface21.beans.factory.annotation.Autowired;
-import com.interface21.context.stereotype.Service;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
-    private final TransactionManager transactionManager;
+    User findByAccount(final String account);
 
-    @Autowired
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final TransactionManager transactionManager) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-        this.transactionManager = transactionManager;
-    }
+    User findById(final long id);
 
-    public User findByAccount(final String account) {
-        return userDao.findByAccount(account);
-    }
+    void save(final User user);
 
-    public User findById(final long id) {
-        return userDao.findById(id);
-    }
+    void changePassword(final long id, final String newPassword, final String createdBy);
 
-    public void save(final User user) {
-        userDao.insert(user);
-    }
-
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        transactionManager.begin();
-        try {
-            final var user = findById(id);
-            user.changePassword(newPassword);
-            userDao.update(user);
-            userHistoryDao.log(new UserHistory(user, createBy));
-
-            transactionManager.commit();
-        } catch (Exception e) {
-            transactionManager.rollback();
-            throw e;
-        } finally {
-            transactionManager.close();
-        }
-    }
 }
