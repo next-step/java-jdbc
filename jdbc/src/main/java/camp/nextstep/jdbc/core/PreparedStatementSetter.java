0c043dad9334b468b4aacf42959eafee644e045e
Dongyoon Lee <nine.cow@gmail.com>
2024-08-22 20:40:36 +0900

    fix: 인터페이스들에 @FunctionalInterface 추가

--- jdbc/src/main/java/camp/nextstep/jdbc/core/PreparedStatementSetter.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/PreparedStatementSetter.java
@@ -3,6 +3,7 @@ package camp.nextstep.jdbc.core;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementSetter {
     void setValues(PreparedStatement psmt) throws SQLException;
 }
--- jdbc/src/main/java/camp/nextstep/jdbc/core/SqlExecuteFunction.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/SqlExecuteFunction.java
@@ -2,6 +2,7 @@ package camp.nextstep.jdbc.core;
 
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface SqlExecuteFunction<T> {
     void run(T t) throws SQLException;
 }
