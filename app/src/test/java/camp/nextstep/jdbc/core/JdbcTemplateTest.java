4573b3b37b1a355f38a82acc111921952b808751
Dongyoon Lee <nine.cow@gmail.com>
2024-08-22 20:40:13 +0900

    test: JdbcTemplateTest.java

---
+++ app/src/test/java/camp/nextstep/jdbc/core/JdbcTemplateTest.java
@@ -0,0 +1,118 @@
+package camp.nextstep.jdbc.core;
+
+import camp.nextstep.config.MyConfiguration;
+import camp.nextstep.domain.User;
+import camp.nextstep.support.jdbc.init.DatabasePopulatorUtils;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+class JdbcTemplateTest {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        final var myConfiguration = new MyConfiguration();
+        final var dataSource = myConfiguration.dataSource();
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        DatabasePopulatorUtils.execute(dataSource);
+        jdbcTemplate.update("insert into users(account, password, email) values('new-account', 'password', 'abc@example.com')");
+    }
+
+    @Test
+    void queryByRowMapper() {
+        final List<User> users = jdbcTemplate.query("select * from users", (RowMapper<User>) this::mapUserRow);
+
+        assertThat(users).isNotEmpty()
+                .first().hasFieldOrPropertyWithValue("email", "hkkang@woowahan.com");
+    }
+
+    @Test
+    void queryByResultSetExtractor() {
+        final ResultSetExtractor<String> resultSetExtractor = resultSet -> resultSet.getString("email");
+
+        final String email = jdbcTemplate.query("select * from users", resultSetExtractor);
+
+        assertThat(email).isEqualTo("hkkang@woowahan.com");
+    }
+
+    @Test
+    void queryByRowMapperAndPreparedStatementSetter() {
+        final PreparedStatementSetter pss = new PreparedStatementSetterImpl(new Object[]{"new-account"});
+
+        final List<User> users = jdbcTemplate.query("select * from users where account = ?", pss, (RowMapper<User>) this::mapUserRow);
+
+        assertThat(users).isNotEmpty()
+                .first().hasFieldOrPropertyWithValue("email", "abc@example.com");
+    }
+
+    @Test
+    void queryByResultSetExtractorAndPreparedStatementSetter() {
+        final PreparedStatementSetter pss = new PreparedStatementSetterImpl(new Object[]{"new-account"});
+
+        final User user = jdbcTemplate.query("select * from users where account = ?", pss, (ResultSetExtractor<User>) this::mapUserRow);
+
+        assertThat(user).hasFieldOrPropertyWithValue("email", "abc@example.com");
+    }
+
+    @Test
+    void testInsertionWithVarargs() {
+        final String givenAccount = "new-account";
+        final String givenPassword = "new-password";
+        final String givenEmail = "test@abc.com";
+
+        final int current = findAllUser().size();
+
+        jdbcTemplate.update("insert into users(account, password, email) values(?,?,?)",
+                            givenAccount, givenPassword, givenEmail);
+
+        assertThat(findAllUser()).hasSize(current + 1);
+        assertThat(findLastUser()).hasFieldOrPropertyWithValue("account", givenAccount)
+                .hasFieldOrPropertyWithValue("password", givenPassword)
+                .hasFieldOrPropertyWithValue("email", givenEmail);
+    }
+
+    @Test
+    void testUpdateWithPreparedStatementSetter() {
+        final int current = findAllUser().size();
+
+        final String givenAccount = "some-account";
+        final String givenPassword = "newpassword";
+
+        jdbcTemplate.update("update users set account=?, password=? where id=?",
+                            new PreparedStatementSetterImpl(new Object[]{givenAccount, givenPassword, 1L}));
+
+        assertThat(findAllUser()).hasSize(current);
+
+        assertThat(findUserById(1L))
+                .hasFieldOrPropertyWithValue("id", 1L)
+                .hasFieldOrPropertyWithValue("account", givenAccount)
+                .hasFieldOrPropertyWithValue("password", givenPassword);
+    }
+
+    private User mapUserRow(final ResultSet resultSet) throws SQLException {
+        return new User(
+                resultSet.getLong("id"),
+                resultSet.getString("account"),
+                resultSet.getString("password"),
+                resultSet.getString("email"));
+    }
+
+    private List<User> findAllUser() {
+        return jdbcTemplate.query("select * from users", (RowMapper<User>) this::mapUserRow);
+    }
+
+    private User findUserById(@SuppressWarnings("SameParameterValue") final long id) {
+        return jdbcTemplate.query("select * from users where id = ?", (ResultSetExtractor<User>) this::mapUserRow, id);
+    }
+
+    private User findLastUser() {
+        return jdbcTemplate.query("select * from users order by id desc limit 1", (ResultSetExtractor<User>) this::mapUserRow);
+    }
+}
--- jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
@@ -21,97 +21,52 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
-        return doQuery(
-                sql,
-                (psmt) -> {
-                    for (int i = 0; i < parameters.length; i++) {
-                        psmt.setObject(i + 1, parameters[i]);
-                    }
-                    final ResultSet resultSet = psmt.executeQuery();
-                    final List<T> result = new ArrayList<>();
-                    while (resultSet.next()) {
-                        result.add(rowMapper.mapRow(resultSet));
-                    }
-                    return result;
-                }
-        );
-    }
-
     public <T> List<T> query(final String sql, final PreparedStatementSetter pss, final RowMapper<T> rowMapper) {
-        return doQuery(
-                sql,
-                (psmt) -> {
-                    pss.setValues(psmt);
-                    final ResultSet resultSet = psmt.executeQuery();
-                    final List<T> result = new ArrayList<>();
-                    while (resultSet.next()) {
-                        result.add(rowMapper.mapRow(resultSet));
-                    }
-                    return result;
-                }
-        );
+        return doQuery(sql, psmt -> {
+            pss.setValues(psmt);
+            final ResultSet resultSet = psmt.executeQuery();
+            final List<T> result = new ArrayList<>();
+            while (resultSet.next()) {
+                result.add(rowMapper.mapRow(resultSet));
+            }
+            return result;
+        });
     }
 
-    public <T> T query(final String sql, final ResultSetExtractor<T> resultSetExtractor, final Object... parameters) {
-        return doQuery(
-                sql,
-                (psmt) -> {
-                    for (int i = 0; i < parameters.length; i++) {
-                        psmt.setObject(i + 1, parameters[i]);
-                    }
-                    final ResultSet resultSet = psmt.executeQuery();
-                    if (resultSet.next()) {
-                        return resultSetExtractor.extractData(resultSet);
-                    }
-                    return null;
-                }
-        );
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
+        return this.query(sql, new PreparedStatementSetterImpl(parameters), rowMapper);
     }
 
     public <T> T query(final String sql, final PreparedStatementSetter pss,
                        final ResultSetExtractor<T> resultSetExtractor) {
-        return doQuery(
-                sql,
-                (psmt) -> {
-                    pss.setValues(psmt);
-                    final ResultSet resultSet = psmt.executeQuery();
-                    if (resultSet.next()) {
-                        return resultSetExtractor.extractData(resultSet);
-                    }
-                    return null;
-                }
-        );
+        return doQuery(sql, psmt -> {
+            pss.setValues(psmt);
+            final ResultSet resultSet = psmt.executeQuery();
+            if (resultSet.next()) {
+                return resultSetExtractor.extractData(resultSet);
+            }
+            return null;
+        });
     }
 
-    public void update(final String sql, final Object... parameters) {
-        doExecute(
-                sql,
-                (psmt) -> {
-                    for (int i = 0; i < parameters.length; i++) {
-                        psmt.setObject(i + 1, parameters[i]);
-                    }
-                    psmt.executeUpdate();
-                }
-        );
+    public <T> T query(final String sql, final ResultSetExtractor<T> resultSetExtractor, final Object... parameters) {
+        return query(sql, new PreparedStatementSetterImpl(parameters), resultSetExtractor);
     }
 
     public void update(final String sql, final PreparedStatementSetter pss) {
-        doExecute(
-                sql,
-                (psmt) -> {
-                    pss.setValues(psmt);
-                    psmt.executeUpdate();
-                }
-        );
+        doExecute(sql, psmt -> {
+            pss.setValues(psmt);
+            psmt.executeUpdate();
+        });
     }
 
-    private <T> T doQuery(final String sql,
-                          final SqlQueryFunction<PreparedStatement, T> sqlQueryFunction) {
-        try (
-                final Connection conn = dataSource.getConnection();
-                final PreparedStatement psmt = conn.prepareStatement(sql)
-        ) {
+    public void update(final String sql, final Object... parameters) {
+        update(sql, new PreparedStatementSetterImpl(parameters));
+    }
+
+    private <T> T doQuery(final String sql, final SqlQueryFunction<PreparedStatement, T> sqlQueryFunction) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement psmt = conn.prepareStatement(sql)) {
             return sqlQueryFunction.apply(psmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -120,10 +75,8 @@ public class JdbcTemplate {
     }
 
     private void doExecute(final String sql, final SqlExecuteFunction<PreparedStatement> sqlExecuteFunction) {
-        try (
-                final Connection conn = dataSource.getConnection();
-                final PreparedStatement psmt = conn.prepareStatement(sql)
-        ) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement psmt = conn.prepareStatement(sql)) {
             sqlExecuteFunction.run(psmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
