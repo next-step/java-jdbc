a4aea71cf0e683eab118e15db7aab7ed337c1040
Jivebreaddev <jivebreaddev@gmail.com>
2024-08-25 23:30:37 +0900

    test(UserServiceTest): UserServiceTest에 대해서, TxUserService를 적용합니다.

--- app/src/test/java/camp/nextstep/service/UserServiceTest.java
+++ app/src/test/java/camp/nextstep/service/UserServiceTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 class UserServiceTest {
 
     private JdbcTemplate jdbcTemplate;
@@ -37,8 +38,8 @@ class UserServiceTest {
     @Test
     void testChangePassword() {
         final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao, new PlatformTransactionalManager(), dataSource);
-
+        final var userService = new AppUserService(userDao, userHistoryDao);
+        final var txUserService = new TxUserService(userService, new PlatformTransactionalManager(), dataSource);
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
         userService.changePassword(1L, newPassword, createBy);
@@ -52,13 +53,14 @@ class UserServiceTest {
     void testTransactionRollback() {
         // 트랜잭션 롤백 테스트를 위해 mock으로 교체
         final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao, new PlatformTransactionalManager(), dataSource);
-
+        final var userService = new AppUserService(userDao, userHistoryDao);
+        final var txUserService = new TxUserService(userService, new PlatformTransactionalManager(), dataSource);
         final var newPassword = "newPassword";
         final var createBy = "gugu";
+
         // 트랜잭션이 정상 동작하는지 확인하기 위해 의도적으로 MockUserHistoryDao에서 예외를 발생시킨다.
         assertThrows(DataAccessException.class,
-            () -> userService.changePassword(1L, newPassword, createBy));
+            () -> txUserService.changePassword(1L, newPassword, createBy));
 
         final var actual = userService.findById(1L);
 
