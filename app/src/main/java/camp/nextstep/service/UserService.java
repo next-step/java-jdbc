6a6d8e90ca6043d16543d78ddbcf430c43357156
youngbinary <youngbin@barogo.com>
2024-08-28 22:24:09 +0900

    feat: userDao 와 userHistoryDao 를 한 트랜잭션으로 묶는다

--- app/src/main/java/camp/nextstep/service/UserService.java
+++ app/src/main/java/camp/nextstep/service/UserService.java
@@ -4,6 +4,8 @@ import camp.nextstep.dao.UserDao;
 import camp.nextstep.dao.UserHistoryDao;
 import camp.nextstep.domain.User;
 import camp.nextstep.domain.UserHistory;
+import camp.nextstep.transaction.Transaction;
+import camp.nextstep.transaction.TransactionManager;
 import com.interface21.beans.factory.annotation.Autowired;
 import com.interface21.context.stereotype.Service;
 
@@ -12,11 +14,13 @@ public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final TransactionManager transactionManager;
 
     @Autowired
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, TransactionManager transactionManager) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.transactionManager = transactionManager;
     }
 
     public User findByAccount(final String account) {
@@ -32,9 +36,12 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
+      try(Transaction transaction = transactionManager.begin()) {
         final var user = findById(id);
         user.changePassword(newPassword);
         userDao.update(user);
         userHistoryDao.log(new UserHistory(user, createBy));
+        transaction.commit();
+      }
     }
 }
