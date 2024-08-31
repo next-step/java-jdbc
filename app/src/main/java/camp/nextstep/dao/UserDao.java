120b667aef6662b686f8128a5e1bfbb73092f01e
jeongho <qkr2435@gmail.com>
2024-08-31 23:05:47 +0900

    feat: userDao 매핑 메소드 추가

--- app/src/main/java/camp/nextstep/dao/UserDao.java
+++ app/src/main/java/camp/nextstep/dao/UserDao.java
@@ -4,15 +4,13 @@ import camp.nextstep.domain.User;
 import camp.nextstep.jdbc.core.JdbcTemplate;
 import com.interface21.beans.factory.annotation.Autowired;
 import com.interface21.context.stereotype.Repository;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
+import java.util.Collections;
 
 @Repository
 public class UserDao {
@@ -25,28 +23,43 @@ public class UserDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void insert(final User user) {
+    public void insert2(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        jdbcTemplate.update(sql, List.of(user.getAccount(), user.getPassword(), user.getEmail()));
+    }
 
-        jdbcTemplate.insert();
+
+    public void insert(final User user) {
+        final var sql = "INSERT INTO users (account, password, email) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, List.of(user.getAccount(), user.getPassword(), user.getEmail()));
     }
 
     public void update(final User user) {
-        // todo
+        final var sql = "UPDATE users SET account = ?, password = ?, email = ? WHERE id = ?";
+        jdbcTemplate.update(sql, List.of(user.getAccount(), user.getPassword(), user.getEmail(), user.getId()));
     }
 
     public List<User> findAll() {
-        String sql = "select * from users";
-        return jdbcTemplate.selectAll(sql);
+        String sql = "SELECT * FROM users";
+        return jdbcTemplate.queryForList(sql, Collections.emptyList(), this::mapRowToUser);
     }
 
     public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
-        return null;
+        final var sql = "SELECT id, account, password, email FROM users WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, List.of(id), this::mapRowToUser);
     }
 
     public User findByAccount(final String account) {
-        // todo
-        return null;
+        final var sql = "SELECT id, account, password, email FROM users WHERE account = ?";
+        return jdbcTemplate.queryForObject(sql, List.of(account), this::mapRowToUser);
+    }
+
+    private User mapRowToUser(ResultSet rs) throws SQLException {
+        return new User(
+            rs.getLong("id"),
+            rs.getString("account"),
+            rs.getString("password"),
+            rs.getString("email")
+        );
     }
 }
