72a1e5d1afec86286d1e07dfb00fa27008648931
jeongho <qkr2435@gmail.com>
2024-08-31 22:34:28 +0900

    feat: queryForObject 구현

--- jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
@@ -40,18 +40,31 @@ public class JdbcTemplate {
       setParameters(pstmt, params);
       pstmt.executeUpdate();
       try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
-          return generatedKeys.getLong(1);
+        return generatedKeys.getLong(1);
       }
     } catch (SQLException e) {
-      log.error(e.getMessage(), e);
       throw new RuntimeException(e);
     }
   }
-  public <T> T selectAll(String sql) {
-    return null;
+
+  public <T> T queryForObject(String sql, List<?> params, ResultSetHandler<T> resultSetHandler) {
+    try (Connection conn = dataSource.getConnection();
+        PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+      setParameters(preparedStatement, params);
+      try (ResultSet resultSet = preparedStatement.executeQuery()) {
+        if (resultSet.next()) {
+          return resultSetHandler.handle(resultSet);
+        } else {
+          throw new RuntimeException("쿼리 결과가 없습니다.");
+        }
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  public <T> List<T> selectAll(String sql, List<?> params, ResultSetHandler<T> resultSetHandler) {
+  public <T> List<T> queryForList(String sql, List<?> params,
+      ResultSetHandler<T> resultSetHandler) {
     try (Connection conn = dataSource.getConnection();
         PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
 
@@ -65,12 +78,12 @@ public class JdbcTemplate {
         return results;
       }
     } catch (SQLException e) {
-      log.error(e.getMessage(), e);
       throw new RuntimeException(e);
     }
   }
 
-  private void setParameters(PreparedStatement preparedStatement, List<?> params) throws SQLException {
+  private void setParameters(PreparedStatement preparedStatement, List<?> params)
+      throws SQLException {
     for (int i = 0; i < params.size(); i++) {
       preparedStatement.setObject(i + 1, params.get(i));
     }
