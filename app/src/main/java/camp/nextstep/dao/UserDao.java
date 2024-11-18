bb56a6c2d6eebf3dbcc02cbc91fb0be547a3cb5c
newvelop <76109949+newvelop@users.noreply.github.com>
2024-11-18 17:47:40 +0900

    step2 리팩터링 (#46)
    * refactor : preparestatementsetter 구현
    
    * pr 리뷰 반영

--- app/src/main/java/camp/nextstep/dao/UserDao.java
+++ app/src/main/java/camp/nextstep/dao/UserDao.java
@@ -29,12 +29,12 @@ public class UserDao {
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-        jdbcTemplate.insert(sql, List.of(user.getAccount(), user.getPassword(), user.getEmail()));
+        jdbcTemplate.insert(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
         final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
-        jdbcTemplate.update(sql, List.of(user.getAccount(), user.getPassword(), user.getEmail(), user.getId()));
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
@@ -44,17 +44,17 @@ public class UserDao {
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(sql, allColumnRowMapper(), List.of(id));
+        return jdbcTemplate.queryForObject(sql, allColumnRowMapper(), id);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
-        return jdbcTemplate.queryForObject(sql, allColumnRowMapper(), List.of(account));
+        return jdbcTemplate.queryForObject(sql, allColumnRowMapper(), account);
     }
 
     public void deleteByAccount(final String account) {
         final var sql = "delete from users where account = ?";
-        jdbcTemplate.delete(sql, List.of(account));
+        jdbcTemplate.delete(sql, account);
     }
 
     private RowMapper<User> allColumnRowMapper() {
--- jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
@@ -20,111 +20,87 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void insert(String sql, List<Object> params) {
-        Connection conn = getConnection();
-        runUpdateQuery(conn, sql, params);
+    public void insert(String sql, Object... params) {
+        try (Connection conn = getConnection()) {
+            runUpdateQuery(conn, sql, params);
+        } catch (SQLException e) {
+            log.error("sql exception", e);
+        }
     }
 
-    public void update(String sql, List<Object> params) {
-        Connection conn = getConnection();
-        runUpdateQuery(conn, sql, params);
+    public void update(String sql, Object... params) {
+        try (Connection conn = getConnection()) {
+            runUpdateQuery(conn, sql, params);
+        } catch (SQLException e) {
+            log.error("sql exception", e);
+        }
     }
 
-    public void delete(String sql, List<Object> params) {
-        Connection conn = getConnection();
-        runUpdateQuery(conn, sql, params);
+    public void delete(String sql, Object... params) {
+        try (Connection conn = getConnection()) {
+            runUpdateQuery(conn, sql, params);
+        } catch (SQLException e) {
+            log.error("sql exception", e);
+        }
     }
 
-    public <T> T queryForObject(final String sql, final RowMapper<T> rm) {
-        return queryForObject(sql, rm, new ArrayList<>());
-    }
-
-    public <T> T queryForObject(final String sql, final RowMapper<T> rm, final List<Object> params) {
+    public <T> T queryForObject(final String sql, final RowMapper<T> rm, final Object... params) {
         final var list = query(sql, rm, params);
         return list == null || list.isEmpty() ? null : list.get(0);
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rm) {
-        return query(sql, rm, new ArrayList<>());
-    }
-
-    public <T> List<T> query(final String sql, final RowMapper<T> rm, final List<Object> params) {
-        PreparedStatement pstmt = null;
-        Connection conn = getConnection();
-        List<T> result = new ArrayList<>();
-        try {
-            pstmt = conn.prepareStatement(sql);
-
+    public <T> List<T> query(final String sql, final RowMapper<T> rm, final Object... params) {
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+         ) {
+            List<T> result = new ArrayList<>();
             log.debug("query : {}", sql);
-            for (int i = 0; i < params.size(); i++) {
-                pstmt.setString(i + 1, params.get(i).toString());
-            }
+            defaultPreparedStatementSetter(params).setValues(pstmt);
             final var resultSet = pstmt.executeQuery();
-            while (true) {
+            while (resultSet.next()) {
                 try {
-                    if (!resultSet.next()) break;
                     result.add(rm.mapRow(resultSet));
                 } catch (SQLException e) {
-                    e.printStackTrace();
+                    log.error("sqlexception", e);
                     break;
                 }
             }
+            return result;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-            return result;
         }
     }
 
-    private void runUpdateQuery(Connection conn, String sql, List<Object> params) {
-        PreparedStatement pstmt = null;
-        try {
-            pstmt = conn.prepareStatement(sql);
-
+    private void runUpdateQuery(Connection conn, String sql, Object... params) {
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             log.debug("query : {}", sql);
-            for (int i = 0; i < params.size(); i++) {
-                pstmt.setString(i + 1, params.get(i).toString());
-            }
+            defaultPreparedStatementSetter(params).setValues(pstmt);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
         }
     }
 
     private Connection getConnection() {
-        Connection conn = null;
         try {
             return dataSource.getConnection();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
+    }
+
+    private PreparedStatementSetter defaultPreparedStatementSetter(Object... args) throws SQLException {
+        return ps -> {
+            try {
+                for (int i = 0; i < args.length; i++) {
+                    ps.setObject(i + 1, args[i]);
+                }
+            } catch (NullPointerException | SQLException e) {
+                throw new SQLException(e);
+            }
+        };
     }
 }
